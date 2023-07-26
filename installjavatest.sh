785ce6a031aadbe88aa13bb295337f783f51f951
RenzCool <130404473+RenzCool@users.noreply.github.com>
2023-07-27 07:19:58 +0800

    Update installjavatest.sh

--- installjavatest.sh
+++ installjavatest.sh
@@ -1,65 +1,59 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!&PREFIX/bin/ sh
 
-#Setup
-set -e
-shopt -s expand_aliases
-alias ee='echo -e'
-
-#Greetings
-echo
-ee "\e[93mThis script will install Java in Termux."
-ee "\e[93mLibraries compiled by \e[32mRenz\e[93m, script written by \e[32mRenz \e[93mand \e[32mMyself\e[93m."
-echo
-
-#Checking for existing Java installation
-if [ -e $PREFIX/bin/java ]
+if ! hash wget > /dev/null
 then
-	ee "\e[32mJava is already installed!"
-	echo
-	exit
-else
-	#Checking, whether is someone trying to cheat and simplyfy their installation it on Linux (i.e. x86 (not listad, as you can see) machine) using this script, which have no reason to work.
-	case `dpkg --print-architecture` in
-	aarch64)
-		archaname="aarch64"; tag="v17.0.8" ;;
-	arm64)
-		archaname="aarch64"; tag="v17.0.8" ;;
-	armhf)
-		archaname="arm"; tag="v17.0.8" ;;
-	armv7l)
-		archaname="arm"; tag="v17.0.8" ;;
-	arm)
-		archaname="arm"; tag="v17.0.8" ;;
-	*)
-		ee "\e[91mERROR: Unknown architecture."; echo; exit ;;
-	esac
-
-	#Actual installation
-	ee "\e[32m[*] \e[34mDownloading JDK-17 (~173Mb) for ${archaname}...  🕛This will take some time, so better make a coffee.🕛"
-	wget https://github.com/RenzCool/termux-jdk-test/releases/download/${tag}/jdk-17.0.8_linux${archaname}_bin.tar.gz -q
-
-	ee "\e[32m[*] \e[34mMoving JDK to system..."
-	mv jdk-17.0.8_linux${archaname}_bin.tar.gz $PREFIX/share
-
-	ee "\e[32m[*] \e[34mExtracting JDK..."
-	cd $PREFIX/share
-	tar -xhf jdk-17.0.8_linux${archaname}_bin.tar.gz
-
-	ee "\e[32m[*] \e[34mSeting-up %JAVA_HOME%..."
-	export JAVA_HOME=$PREFIX/share/jdk-17.0.8
-	echo "export JAVA_HOME=$PREFIX/share/jdk-17.0.8" >> $HOME/.profile
-
-	ee "\e[32m[*] \e[34mCoping Java wrapper scripts to bin..."
-	#I'm not 100% sure, but getting rid of bin contnent MAY cause some issues with %JAVA_HOME%, thus it's no longer moved - copied instead. Sorry to everyone short on storage.
-	cp bin/* $PREFIX/bin
-
-	ee "\e[32m[*] \e[34mCleaning up temporary files..."
-	rm -rf $HOME/installjava
-	rm -rf $PREFIX/share/jdk-17.0.8_linux${archaname}_bin.tar.gz
-	rm -rf $PREFIX/share/bin
-
-	echo
-	ee "\e[32mJava was successfully installed!\e[39m"
-	echo "Enjoy your new, tasty Java :D (and a coffee, if you didn't drink it yet)"
-	echo
+    apt install wget -y
 fi
+
+print_status() {
+    printf "(>) ${1}...\n"
+}
+
+set_arch() {
+    case "$(uname -m)" in
+        aarch64|armv8l)
+            ARCH=aarch64
+            ;;
+        armv7l|arm)
+            ARCH=arm
+            ;;
+        *)
+            printf "(!) Your device architecture not supported yet.\n"
+            exit 1
+            ;;
+    esac
+}
+
+get_tar() {
+    wget -q --show-progress -c https://github.com/Hax4us/java/releases/download/v8/jdk-17.0.8_linux-${ARCH}_bin.tar.gz -O jdk-17.0.8_linux-${ARCH}_bin.tar.gz
+    tar -xf jdk-17.0.8_linux-${ARCH}_bin.tar.gz -C $PREFIX/share 
+    chmod +x $PREFIX/share/bin/*
+}
+
+get_java()
+{
+cat <<- CONF > $PREFIX/bin/java
+#!/usr/bin/bash
+
+unset LD_PRELOAD
+export JAVA_HOME="$PREFIX/share/jdk-17.0.8"
+
+export LIB_DIR="\$PREFIX/share/glib"
+export LD_LIBRARY_PATH="\$LIB_DIR"
+exec proot -0 \$JAVA_HOME/bin/java "\$@"
+CONF
+}
+
+cleanup() {
+    rm -f jdk-17.0.8_linux-${ARCH}_bin.tar.gz
+    rm -rf $PREFIX/share/bin
+}
+
+set_arch
+print_status "Getting system architecture"
+get_tar
+print_status "Extracting tar files to specific directory"
+print_status "Setting up java program for execution"
+get_java
+print_status "Cleaning up unwanted files"
+cleanup
